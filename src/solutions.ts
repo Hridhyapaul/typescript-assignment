f8cc0bb0df9d2f2b5963e9f39befae6c248daadf
Hridhyapaul <hpdesign2021@gmail.com>
2026-05-14 22:00:15 +0600

    Solution 02 and Solution 03

--- src/solutions.ts
+++ src/solutions.ts
@@ -6,3 +6,28 @@ function filterEvenNumbers(numbers: number[]): number[] {
 
 const numbers1 = [1, 2, 3, 4, 5, 6];
 console.log(filterEvenNumbers(numbers1));
+
+// ========= Solution 02 =========
+
+function reverseString(text: string): string {
+    return text.split('').reverse().join('');
+}
+
+console.log(reverseString("typescript"));
+
+// ========= Solution 03 =========
+
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): string {
+    if (typeof value === "string") {
+        return "String";
+    } else if (typeof value === "number") {
+        return "Number";
+    }
+}
+
+console.log(checkType("Hello"));
+console.log(checkType(42));
+
+// ========= Solution 04 =========
